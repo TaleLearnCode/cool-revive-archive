e487c77e140858006e794b03aff3728b72cc4bf0
Chad Green <chadgreen@chadgreen.com>
2024-07-28 13:04:56 -0400

    Lab 1 completed

---
+++ src/Core/Messages/Messages/IMessage.cs
@@ -0,0 +1,18 @@
+﻿namespace Remanufacturing.Messages;
+
+/// <summary>
+/// Interface for Cool Revive Remanufacturing messages.
+/// </summary>
+public interface IMessage
+{
+
+	/// <summary>
+	/// Gets or sets the tracking identifier for the message.
+	/// </summary>
+	string MessageId { get; set; }
+
+	/// <summary>
+	/// Gets or sets the type of the message.
+	/// </summary>
+	string MessageType { get; set; }
+}
---
+++ src/Core/Messages/Messages/OrderNextCoreMessage.cs
@@ -0,0 +1,11 @@
+﻿namespace Remanufacturing.Messages;
+
+public class OrderNextCoreMessage : IMessage
+{
+	public string MessageId { get; set; } = Guid.NewGuid().ToString();
+	public string MessageType { get; set; } = MessageTypes.OrderNextCore;
+	public string PodId { get; set; } = null!;
+	public string? CoreId { get; set; }
+	public string? FinishedProductId { get; set; }
+	public DateTime RequestDateTime { get; set; }
+}
